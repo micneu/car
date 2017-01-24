d71b35d9e139daace609e649965039b1ce5bd2df
micneu <michael.neu@t-online.de>
2017-01-24 13:48:07 +0100

    Create driveman.py
    initial

---
+++ driveman.py
@@ -0,0 +1,8 @@
+# This manages the drive functions of the car
+# The list of drive functions is:
+# - accellerate forward
+# - accellerate backward
+# - steer left
+# - steer right
+# - steer straight
+# - brake
